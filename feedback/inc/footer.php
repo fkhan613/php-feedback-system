90f55f5ffcd7aa76a9f72ba6a0ce3cee6e47bd25
Farhan Khan <farhan.k2005@gmail.com>
2022-06-12 19:45:24 -0400

    added the php pages

---
+++ feedback/inc/footer.php
@@ -0,0 +1,3 @@
+<footer class="text-center mt-5">
+  Copyright &copy; 2022
+</footer>
---
+++ feedback/index.php
@@ -0,0 +1,31 @@
+<?php include "inc/header.php" ?>
+
+    <img src="/php-feedback-system/feedback/img/PFP.png" class="w-25 mb-3" alt="">
+    <h2>Feedback</h2>
+    <p class="lead text-center">Leave feedback for Khan Media</p>
+    <form action="" class="mt-4 w-75">
+      <div class="mb-3">
+        <label for="name" class="form-label">Name</label>
+        <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name">
+      </div>
+      <div class="mb-3">
+        <label for="email" class="form-label">Email</label>
+        <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email">
+      </div>
+      <div class="mb-3">
+        <label for="body" class="form-label">Feedback</label>
+        <textarea class="form-control" id="body" name="body" placeholder="Enter your feedback"></textarea>
+      </div>
+      <div class="mb-3">
+        <input type="submit" name="submit" value="Send" class="btn btn-dark w-100">
+      </div>
+    </form>
+    </div>
+</main>
+
+<?php include "inc/footer.php"?>
+ 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+</body>
+</html>
