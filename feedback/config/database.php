23c64abdfa90770ab7b27b65f680e438decb5361
Farhan Khan <farhan.k2005@gmail.com>
2022-06-13 09:38:02 -0400

    changed database

--- feedback/config/database.php
+++ feedback/config/database.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'farhan');
-define('DB_pass', '123456');
-define('DB_NAME', 'php_dev');
+define('DB_HOST', 'sql201.epizy.com');
+define('DB_USER', 'epiz_31947149');
+define('DB_pass', 'monaxDMLx5');
+define('DB_NAME', 'epiz_31947149_php_dev');
 
 //create connection
 $conn = mysqli_connect(DB_HOST, DB_USER, DB_pass, DB_NAME);
