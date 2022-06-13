509f1555af941b1c2fd0af1fedb3988242ea4ec6
Farhan Khan <farhan.k2005@gmail.com>
2022-06-13 18:13:40 -0400

    chnaged href for logo

--- feedback/inc/header.php
+++ feedback/inc/header.php
@@ -11,7 +11,7 @@
 <body>
   <nav class="navbar navbar-expand-sm navbar-light bg-light mb-4">
     <div class="container">
-      <a class="navbar-brand" href="#">Khan Media</a>
+      <a class="navbar-brand" href="index.php">Khan Media</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
